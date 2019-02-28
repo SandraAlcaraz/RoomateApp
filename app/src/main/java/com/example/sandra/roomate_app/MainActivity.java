10c7c6e0834c945dbb50b359eb6d616e36f0c0cb
alfonsoirai <pon.alfonsoirai@gmail.com>
2019-02-27 23:16:45 -0600

    Added UI Elements and fixed logout issue

--- app/src/main/java/com/example/sandra/roomate_app/MainActivity.java
+++ app/src/main/java/com/example/sandra/roomate_app/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.sandra.roomate_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Home");
+        logoutBtn = findViewById(R.id.logout_btn);
     }
+
+    public void sessionLogout(View v){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+
     @Override
     public void onBackPressed() {
         Intent setIntent = new Intent(Intent.ACTION_MAIN);
