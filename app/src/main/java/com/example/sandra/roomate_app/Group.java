543c24ed4f3f2289d8dc86381af5952b8d9ac1cf
alfonsoirai <pon.alfonsoirai@gmail.com>
2019-02-28 01:08:01 -0600

    Add user and group class

--- app/src/main/java/com/example/sandra/roomate_app/Group.java
+++ app/src/main/java/com/example/sandra/roomate_app/Group.java
@@ -1,4 +1,30 @@
 package com.example.sandra.roomate_app;
 
+import java.util.ArrayList;
+
 public class Group {
+    private String code, name;
+    private ArrayList<User> users;
+
+    public Group(String code, String name, ArrayList<User> users){
+        this.code = code;
+        this.name = name;
+        this.users = users;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ArrayList<User> getUsers(){
+        return this.users;
+    }
+
+    public int numberOfUsers(){
+        return this.users.size();
+    }
 }
--- app/src/main/java/com/example/sandra/roomate_app/User.java
+++ app/src/main/java/com/example/sandra/roomate_app/User.java
@@ -1,4 +1,18 @@
 package com.example.sandra.roomate_app;
 
 public class User {
+    private String id, email, name, groupId;
+
+    public User(String id, String email){
+        this.id = id;
+        this.email = email;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setGroupId(String groupId){
+        this.groupId = groupId;
+    }
 }
