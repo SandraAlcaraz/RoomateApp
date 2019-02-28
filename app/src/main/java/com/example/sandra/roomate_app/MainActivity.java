6e94544e06d213407390e9286dbc302fa0f77b42
SandraAlcaraz <sandra_alcaraz.a@outlook.com>
2019-02-27 19:43:41 -0600

    Login-Register-Done

---
+++ app/src/main/java/com/example/sandra/roomate_app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sandra.roomate_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onBackPressed() {
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+    }
+}
