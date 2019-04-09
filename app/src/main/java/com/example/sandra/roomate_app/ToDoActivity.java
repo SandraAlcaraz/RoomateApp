b0208ad2eac5558eaa168c21d09f51cc868aaa99
Floreth Gonzalez <florethgonzalez@Floreths-MacBook-Pro.local>
2019-04-08 22:21:56 -0500

    Menu transitions

---
+++ app/src/main/java/com/example/sandra/roomate_app/ToDoActivity.java
@@ -0,0 +1,15 @@
+package com.example.sandra.roomate_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ToDoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        setTitle("To Do");
+    }
+
+}
+
