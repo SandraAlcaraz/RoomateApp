5e9d4a7339e4c0c1a0189316976caed39a2d0850
SandraAlcaraz <sandra_alcaraz.a@outlook.com>
2019-04-11 06:09:37 -0500

    adding display todo

--- app/src/main/java/com/example/sandra/roomate_app/ToDoActivity.java
+++ app/src/main/java/com/example/sandra/roomate_app/ToDoActivity.java
@@ -1,6 +1,9 @@
 package com.example.sandra.roomate_app;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,8 +11,11 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -21,6 +27,8 @@ import java.util.List;
 public class ToDoActivity extends AppCompatActivity {
     private DatabaseReference Db;
     private ListView todosListView;
+    Todo [] todosArray;
+    SimpleAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +37,53 @@ public class ToDoActivity extends AppCompatActivity {
         todosListView= (ListView) findViewById(R.id.todoListView);
 
        Db = FirebaseDatabase.getInstance().getReference().child("Tasks");
-        Todo todo1= new Todo("id1","lavar","0001",00002,00003, Calendar.getInstance().getTime(),Calendar.getInstance().getTime(), "Marua");
+        Todo todo1= new Todo("id1","lavar","0001","me", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(), "Marua");
 
-        Todo todo2= new Todo("id1","planchar","0001",00002,00003, Calendar.getInstance().getTime(),Calendar.getInstance().getTime(), "Juan");
+        Todo todo2= new Todo("id1","planchar","0001","me", Calendar.getInstance().getTime(),Calendar.getInstance().getTime(), "Juan");
 
-        Todo [] todosArray= new Todo[2];
-        todosArray[0]=todo1;
-        todosArray[1]=todo2;
-        SimpleAdapter adapter= getTodosAdapter(todosArray);
-        todosListView.setAdapter(adapter);
-        setListViewHeightBasedOnChildren(todosListView);
-                                               
+
+
+
+
+        Db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List notes = new ArrayList<>();
+                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
+
+                    String desAr=noteDataSnapshot.child("description").getValue().toString();
+                    String assigAr=noteDataSnapshot.child("assigneeName").getValue().toString();
+                    String createdAr=noteDataSnapshot.child("createdBy").getValue().toString();
+                    Boolean finished= Boolean.valueOf(noteDataSnapshot.child("finished").getValue().toString());
+
+                    //Todo todoItem = noteDataSnapshot.getValue(Todo.class);
+                    Todo todoItem= new Todo();
+                    todoItem.setDescription(desAr);
+                    todoItem.setAssigneeName(assigAr);
+                    todoItem.setCreatedBy(createdAr);
+                    todoItem.setFinished(finished);
+                    notes.add(todoItem);
+                }
+
+                todosArray= new Todo[notes.size()];
+               for(int i=0;i<notes.size();i++){
+                   todosArray[i]= (Todo) notes.get(i);
+               }
+                adapter= getTodosAdapter(todosArray);
+                adapter.notifyDataSetChanged();
+                todosListView.setAdapter(adapter);
+                setListViewHeightBasedOnChildren(todosListView);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+
+        });
     }
 
 
@@ -49,13 +93,14 @@ public class ToDoActivity extends AppCompatActivity {
         // create the item mapping
 
         SimpleDateFormat simpleDateFormat= new SimpleDateFormat("E, d  M");
-        String[] from = {"id", "description", "groupId", "assignee", "dueDate"};
+        String[] from = {"id", "description", "groupId", "assignee"};
         int[] to = {
                 R.id.itemTodoId,
                 R.id.itemTodoDesc,
                 R.id.itemTodoGroupId,
                 R.id.itemTodoAssignee,
-                R.id.itemTodoDueDate};
+               // R.id.itemTodoDueDate
+        };
         // prepare the list of all records
         List<HashMap<String, String>> fillMaps = new ArrayList<>();
         for(Todo todo : todos){
@@ -64,7 +109,7 @@ public class ToDoActivity extends AppCompatActivity {
             map.put("description", todo.getDescription());
             map.put("groupId", todo.getGroupId());
             map.put("assignee",  todo.getAssigneeName());
-            map.put("dueDate", simpleDateFormat.format(todo.getDueDate()));
+//            map.put("dueDate", simpleDateFormat.format(todo.getDueDate()));
             fillMaps.add(map);
         }
         return new SimpleAdapter(this,fillMaps,R.layout.item_todo,from,to);
@@ -89,6 +134,15 @@ public class ToDoActivity extends AppCompatActivity {
         ViewGroup.LayoutParams params = listView.getLayoutParams();
         params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
         listView.setLayoutParams(params);
+    }
+
+    public void createTodo(View v){
+        Db = FirebaseDatabase.getInstance().getReference().child("Tasks");
+        DashboardTodo fragment = new DashboardTodo();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.relative,  fragment).addToBackStack(null);
+        transaction.commit();
     }
 }
 
