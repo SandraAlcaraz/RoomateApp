0c0a80530088fa977275546b2598a4a57f69f51e
toniikar <47513743+toniikar@users.noreply.github.com>
2019-04-11 05:36:17 -0500

    Profile Name progress

---
+++ app/src/main/java/com/example/sandra/roomate_app/ProfileActivity.java
@@ -0,0 +1,37 @@
+package com.example.sandra.roomate_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView name, email;
+    private String key;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        name = (TextView) findViewById(R.id.userName);
+        email = (TextView) findViewById(R.id.userEmail);
+
+        Intent intent = getIntent();
+        key = intent.getStringExtra("key");
+
+
+    }
+
+
+}
